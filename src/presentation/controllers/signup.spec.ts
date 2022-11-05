de72ab96de84483b70ac5baa74a23fc60cadb22d
renancorreadev <producer@skynance.net>
2022-11-05 18:42:40 -0300

    :white_check_mark:(test): ensure SignUpController returns 400 if no email provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,18 +1,34 @@
 import { SignUpController } from "./signup";
-describe("first", () => {
+describe("SignupController", () => {
   test("Should return 400 if no name is provided", () => {
     const sut = new SignUpController();
 
     const httpRequest = {
       body: {
-        email: "",
-        password: "",
-        passwordConfirmation: ""
+        email: "any_email",
+        password: "any_password",
+        passwordConfirmation: "any_password"
       }
     };
 
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+  });
+
+  test("Should return 400 if no email is provided", () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password"
+      }
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: Email"));
   });
 });
