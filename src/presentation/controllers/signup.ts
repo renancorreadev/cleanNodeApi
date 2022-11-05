e19a84c9e3e646874b19a29ec2674a149c2185da
renancorreadev <producer@skynance.net>
2022-11-05 18:42:04 -0300

    :sparkles:(feat): ensure SignUpController returns 400 if no email provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,18 @@
 export class SignUpController {
   handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error("Missing param: name")
-    };
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: name")
+      };
+    }
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: Email")
+      };
+    }
   }
 }
