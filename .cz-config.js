5b3e89e6746daad64d8a834785bc1b1e22257f9a
renancorreadev <producer@skynance.net>
2022-11-05 14:04:44 -0300

    :truck:(chore): fix config cz-config.js

--- .cz-config.js
+++ .cz-config.js
@@ -4,33 +4,33 @@ module.exports = {
     {value: ':bug: fix', name: '🐛 fix:\tFixing a bug'},
     {value: ':memo: docs', name: '📝 docs:\tAdd or update documentation'},
     {
-      value: ':lipstick: style',
+      value: ':lipstick: ',
       name: '💄 style:\tAdd or update styles, ui or ux',
     },
     {
-      value: ':recycle: refactor',
+      value: ':recycle: ',
       name: '♻️  refactor:\tCode change that neither fixes a bug nor adds a feature',
     },
     {
-      value: ':zap: perf',
+      value: ':zap: ',
       name: '⚡️ perf:\tCode change that improves performance',
     },
     {
-      value: ':white_check_mark: test',
+      value: ':white_check_mark: ',
       name: '✅ test:\tAdding tests cases',
     },
     {
-      value: ':truck:',
+      value: ':truck: \t ',
       name: '🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation',
     },
-    {value: ':rewind: revert', name: '⏪️ revert:\tRevert to a commit'},
+    {value: ':rewind: ', name: '⏪️ revert:\tRevert to a commit'},
     {value: ':construction: wip', name: '🚧 wip:\tWork in progress'},
     {
-      value: ':construction_worker: build',
+      value: ':construction_worker: ',
       name: '👷 build:\tAdd or update regards to build process',
     },
     {
-      value: ':green_heart: ci',
+      value: ':green_heart: ',
       name: '💚 ci:\tAdd or update regards to build process',
     },
   ],
