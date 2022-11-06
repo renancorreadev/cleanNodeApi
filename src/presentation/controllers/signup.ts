6d56ab3b09426753999cdaa83b6bee26702c0715
renancorreadev <producer@skynance.net>
2022-11-06 01:21:57 -0300

    :recycle:(refactor): create two fields to unduplicated code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,18 +1,17 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { HttpResponse, HttpRequest } from "../protocols/http";
 import { MissingParamError } from "../errors/missing-param-error";
 import { badRequest } from "../helpers/http-helper";
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError("name"));
+    const requiredFields = ["name", "email"];
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError("email"));
-    }
-    return {
-      statusCode: 400
-    };
+
+    return { statusCode: 400 };
   }
 }
