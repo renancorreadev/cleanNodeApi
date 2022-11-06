765e6cc025d77ac85d196c869a64fd7ffe7cb0c2
renancorreadev <producer@skynance.net>
2022-11-06 01:33:52 -0300

    :recycle:(refactor): make SignUpController conforms to a protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,8 @@
 import { HttpResponse, HttpRequest } from "../protocols/http";
 import { MissingParamError } from "../errors/missing-param-error";
 import { badRequest } from "../helpers/http-helper";
-export class SignUpController {
+import { Controller } from "../protocols/controller";
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = [
       "name",
