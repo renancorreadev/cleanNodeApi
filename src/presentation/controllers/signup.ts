318e11f06d427bf33ae5299b18e302bab2d9f85f
renancorreadev <producer@skynance.net>
2022-11-06 02:35:36 -0300

    :recycle:(refactor): move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { HttpResponse, HttpRequest } from "../protocols/http";
 import { MissingParamError } from "../errors/missing-param-error";
-import { badRequest } from "../helpers/http-helper";
+import { badRequest, serverError } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validator";
 import { InvalidParamError } from "../errors/invalid-param-error";
-import { ServerError } from "../errors/server-error";
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -35,10 +34,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError("email"));
       }
     } catch (e) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      };
+      serverError();
     }
 
     return {
