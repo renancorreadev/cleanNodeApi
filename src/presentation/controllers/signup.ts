e8c767c555a0380e5f55b25028759af45dae1e5e
renancorreadev <producer@skynance.net>
2022-11-06 01:14:59 -0300

    :recycle:(refactor): move duplicate code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,20 +1,15 @@
 import { HttpResponse, HttpRequest } from "../protocols/http";
 import { MissingParamError } from "../errors/missing-param-error";
+import { badRequest } from "../helpers/http-helper";
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError("name")
-      };
+      return badRequest(new MissingParamError("name"));
     }
     // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError("email")
-      };
+      return badRequest(new MissingParamError("email"));
     }
     return {
       statusCode: 400
