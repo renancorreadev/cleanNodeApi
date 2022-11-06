cc5e3601dd863a0e70807ff1dc246c86e9737fe0
renancorreadev <producer@skynance.net>
2022-11-06 01:30:21 -0300

    :white_check_mark:(test): ensure SignUpController returns 400 if no password confirmations provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -48,4 +48,22 @@ describe("SignupController", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
+
+  test("Should return 400 if no password confirmation is provided", () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email",
+        password: "any_password"
+      }
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError("passwordConfirmation")
+    );
+  });
 });
