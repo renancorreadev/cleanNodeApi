723621947687b337e79a08f8175649a4e7a3b460
renancorreadev <producer@skynance.net>
2022-11-06 01:29:50 -0300

    :sparkles:(feat): ensure SignUpController returns 400 if no password confirmations provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,12 @@ import { MissingParamError } from "../errors/missing-param-error";
 import { badRequest } from "../helpers/http-helper";
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ["name", "email", "password"];
+    const requiredFields = [
+      "name",
+      "email",
+      "password",
+      "passwordConfirmation"
+    ];
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
