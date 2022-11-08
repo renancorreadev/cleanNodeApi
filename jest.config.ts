f18a5740166a20ed0428bfb9851e6c18c775d524
renancorreadev <producer@skynance.net>
2022-11-08 18:45:13 -0300

    :truck:(chore): add jest configs ands scripts

--- jest.config.ts
+++ jest.config.ts
@@ -4,13 +4,13 @@
  */
 
 export default {
-  roots: ['<rootDir>/src'],
+  roots: ["<rootDir>/src"],
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  testEnvironment: 'node',
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  testEnvironment: "node",
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    ".+\\.ts$": "ts-jest"
   }
-}
+};
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -55,6 +55,7 @@ export class SignUpController implements Controller {
 
       return ok(account);
     } catch (error) {
+      console.error(error);
       return serverError();
     }
   }
