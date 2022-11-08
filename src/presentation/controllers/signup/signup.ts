1281914926ee6ab11304e3b79b9a41783c89f9be
renancorreadev <producer@skynance.net>
2022-11-08 16:42:30 -0300

    :recycle:(refactor): move sucess http response to a helper method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -7,7 +7,7 @@ import {
   AddAccountIFace
 } from "./signup-protocols";
 import { MissingParamError, InvalidParamError } from "../../errors";
-import { badRequest, serverError } from "../../helpers/http-helper";
+import { badRequest, serverError, ok } from "../../helpers/http-helper";
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidatorIFace;
@@ -53,10 +53,7 @@ export class SignUpController implements Controller {
         password
       });
 
-      return {
-        statusCode: 200,
-        body: account
-      };
+      return ok(account);
     } catch (error) {
       return serverError();
     }
