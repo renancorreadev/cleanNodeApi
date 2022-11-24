ecc87069e4c8d4e017c8c85afca372a3892a04ba
renancorreadev <producer@skynance.net>
2022-11-24 00:18:02 -0300

    :white_check_mark:(test): ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usercases/add-account/db-add-account.spec.ts
+++ src/data/usercases/add-account/db-add-account.spec.ts
@@ -116,4 +116,23 @@ describe("DbAccount UseCase", () => {
       password: "hashed_password"
     });
   });
+
+  test("Should throw if Encrypter throws", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(addAccountRepositoryStub, "add")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password"
+    };
+
+    const accountPromise = sut.add(accountData);
+    await expect(accountPromise).rejects.toThrow();
+  });
 });
