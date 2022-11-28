c053b90b65b59b982f0c7e4b6b7cd684a19a69ba
renancorreadev <producer@skynance.net>
2022-11-28 00:24:56 -0300

    :white_check_mark:(test): ensure AccountMongoRepository returns an account on Success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,35 @@
+import { MongoHelper } from "../helpers/mongo-helper";
+import { AccountMongoRepository } from "./account";
+
+/** Test Integration to MongoDB */
+describe("Account Mongo Repository", () => {
+  /** Pre initialize on tests */
+
+  beforeAll(async () => {
+    await MongoHelper.connect(
+      process.env.MONGO_URL != null ? process?.env?.MONGO_URL : ""
+    );
+  });
+
+  /** Pos Initialize on tests */
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test("Should return an account on Sucess", async () => {
+    const sut = new AccountMongoRepository();
+
+    const account = await sut.add({
+      name: "",
+      email: "",
+      password: ""
+    });
+
+    /** toBeTruthy is not undefined or null */
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe("any_name");
+    expect(account.email).toBe("any_email@gmail.com.br");
+    expect(account.password).toBe("any_password");
+  });
+});
