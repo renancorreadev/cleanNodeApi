41c6ccff7eb2b873befa448e7fa58922ea853ac9
renancorreadev <producer@skynance.net>
2022-11-28 01:06:57 -0300

    :sparkles:(feat): ensure request parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,10 +3,7 @@
  * Não necessario desenvolver testes unitarios porem é importante desenvolver testes
  * de integração.
  */
-
-import express from "express";
-
-const app = express();
+import app from "./config/app";
 
 app.listen(5050, () => {
   console.log("Server running at http://localhost:5050");
