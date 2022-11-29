ee9135c93578a4fd31a07d1933c6226a4b2762ca
renancorreadev <producer@skynance.net>
2022-11-28 23:32:11 -0300

    :recycle:(refactor): config commitlint with ts

--- .cz-config.js
+++ .cz-config.js
@@ -1,48 +1,53 @@
 module.exports = {
   types: [
-    {value: ':sparkles: ', name: '✨ feat:\tAdding a new feature'},
-    {value: ':bug: ', name: '🐛 fix:\tFixing a bug'},
-    {value: ':memo: ', name: '📝 docs:\tAdd or update documentation'},
+    { value: ":sparkles: ", name: "✨ feat:\tAdding a new feature" },
+    { value: ":bug: ", name: "🐛 fix:\tFixing a bug" },
+    { value: ":memo: ", name: "📝 docs:\tAdd or update documentation" },
     {
-      value: ':lipstick: ',
-      name: '💄 style:\tAdd or update styles, ui or ux',
+      value: ":lipstick: ",
+      name: "💄 style:\tAdd or update styles, ui or ux"
     },
     {
-      value: ':recycle: ',
-      name: '♻️  refactor:\tCode change that neither fixes a bug nor adds a feature',
+      value: ":recycle: ",
+      name: "♻️  refactor:\tCode change that neither fixes a bug nor adds a feature"
     },
     {
-      value: ':zap: ',
-      name: '⚡️ perf:\tCode change that improves performance',
+      value: ":zap: ",
+      name: "⚡️ perf:\tCode change that improves performance"
     },
     {
-      value: ':white_check_mark: ',
-      name: '✅ test:\tAdding tests cases',
+      value: ":white_check_mark: ",
+      name: "✅ test:\tAdding tests cases"
     },
     {
-      value: ':truck: \t ',
-      name: '🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation',
+      value: ":truck: \t ",
+      name: "🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation"
     },
-    {value: ':rewind: ', name: '⏪️ revert:\tRevert to a commit'},
-    {value: ':construction: wip', name: '🚧 wip:\tWork in progress'},
+    { value: ":rewind: ", name: "⏪️ revert:\tRevert to a commit" },
+    { value: ":construction: wip", name: "🚧 wip:\tWork in progress" },
     {
-      value: ':construction_worker: ',
-      name: '👷 build:\tAdd or update regards to build process',
+      value: ":construction_worker: ",
+      name: "👷 build:\tAdd or update regards to build process"
     },
     {
-      value: ':green_heart: ',
-      name: '💚 ci:\tAdd or update regards to build process',
-    },
+      value: ":green_heart: ",
+      name: "💚 ci:\tAdd or update regards to build process"
+    }
   ],
   scopes: [],
 
   scopeOverrides: {
-    fix: [{name: 'merge'}, {name: 'style'}, {name: 'test'}, {name: 'hotfix'}],
+    fix: [
+      { name: "merge" },
+      { name: "style" },
+      { name: "test" },
+      { name: "hotfix" }
+    ]
   },
 
   allowCustomScopes: false,
-  allowBreakingChanges: ['feat', 'fix'],
+  allowBreakingChanges: ["feat", "fix"],
   // skip any questions you want
-  skipQuestions: ['footer', 'breaking'],
-  subjectLimit: 100,
-}
+  skipQuestions: ["footer", "breaking"],
+  subjectLimit: 100
+};
--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,11 @@
-module.exports = {
+import type { UserConfig } from "@commitlint/types";
+
+const CommitGlobalConfig: UserConfig = {
   extends: ["gitmoji"],
   rules: {
     "header-max-length": [0, "always", 100],
     "scope-case": [0, "always", "pascal-case"]
   }
 };
+
+export default CommitGlobalConfig;
